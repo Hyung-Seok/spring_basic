77aea9d3a69512a14381454aa4af437d43dbe106
Hyung-Seok <dulli1616@kakao.com>
2024-04-23 23:18:41 +0900

    Spring

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -6,14 +6,14 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServieImpl;
+import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
 
 
     public static void main(String[] args) {
         MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServieImpl();
+        OrderService orderService = new OrderServiceImpl();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class OrderServiceTest {
 
     MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServieImpl();
+    OrderService orderService = new OrderServiceImpl();
 
     @Test
     void createOrder(){
